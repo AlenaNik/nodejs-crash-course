c9a41c44f29fd5124e617d3f6f61daa02ae27c19
Alena <alena.nik.digital@gmail.com>
2019-09-08 21:19:04 +0200

    404 page added

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<html><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add products</button></form></html>');
-});
 
-app.use('/product', (req, res, next) => {
-        console.log(req.body);
-        res.redirect('/');
-})
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/', (req, res, next) => {
-    res.send('<h1>Nothing here</h1>');
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
 });
 
 app.listen(3000);
