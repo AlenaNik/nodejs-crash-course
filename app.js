bc462fb89cb1f782175e813181aad97999ce399a
Alena <alena.nik.digital@gmail.com>
2019-09-08 18:20:00 +0200

    changed raw server to express framework

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express');
 
 const app = express();
 
-
-app.use('/new', (req, res) => {
-    res.send('<h1>Hello from express!</h1>');
+app.use('/users', (req, res, next) => {
+    console.log('Slash users');
+    res.send('<h1>Dummy text users</h1>');
 });
+
+app.use('/', (req, res, next) => {
+    console.log('Just slash');
+    res.send('<h1>Nothing here</h1>');
+});
+
 
 
 app.listen(3000);
